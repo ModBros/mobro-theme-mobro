f2941282cdc4bea71f06f081da4111dc8cd3b01f
David Riedl <david.riedl@elements.at>
2021-10-13 11:18:56 +0200

    chore(publish): preparations for publishing v1.

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
 
     output: {
-        path: path.resolve(__dirname, "..", "mobro-desktop", "mobro-data", "public", "theme", "mobro", "dist"),
+        path: path.resolve(__dirname, "dist"),
         filename: "[name].js"
     },
 
